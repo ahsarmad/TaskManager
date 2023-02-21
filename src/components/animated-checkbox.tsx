fab647f5b83f289e331b150395c2873450c127de
Sarmad <afzalsarmad77@gmail.com>
2023-02-20 22:53:18 -0500

    fixed svg animation using bezier property

--- src/components/animated-checkbox.tsx
+++ src/components/animated-checkbox.tsx
@@ -10,13 +10,15 @@ import Animated, {
 } from 'react-native-reanimated'
 import SVG, { Path, Defs, ClipPath, G } from 'react-native-svg'
 
+import AnimatedStroke from './animated-stroke'
+
 const MARGIN = 10
 const vWidth = 64 + MARGIN
 const vHeight = 64 + MARGIN
 const outlineBoxPath =
   'M24 0.5H40C48.5809 0.5 54.4147 2.18067 58.117 5.88299C61.8193 9.58532 63.5 15.4191 63.5 24V40C63.5 48.5809 61.8193 54.4147 58.117 58.117C54.4147 61.8193 48.5809 63.5 40 63.5H24C15.4191 63.5 9.58532 61.8193 5.88299 58.117C2.18067 54.4147 0.5 48.5809 0.5 40V24C0.5 15.4191 2.18067 9.58532 5.88299 5.88299C9.58532 2.18067 15.4191 0.5 24 0.5Z'
 
-const checkMarkPath = 'M16 30.1977C23.1081 38 26 47 26 47C26 47 35 30 72 4'
+const checkMarkPath = 'M16 30.1977C23.1081 38 26 47 26 47C26 47 35 30 70 4'
 
 const AnimatedPath = Animated.createAnimatedComponent(Path)
 
@@ -34,7 +36,8 @@ const AnimatedCheckBox = (props: Props) => {
 
   useEffect(() => {
     progress.value = withTiming(checked ? 1 : 0, {
-      duration: Easing.linear
+      duration: checked ? 300 : 100,
+      easing: Easing.linear
     })
   }, [checked])
 
@@ -68,7 +71,16 @@ const AnimatedCheckBox = (props: Props) => {
         strokeLinecap="round"
         animatedProps={animatedBoxProps}
       />
-      <Path d={checkMarkPath} stroke="black" />
+      {/* <Path d={checkMarkPath} stroke="black" /> */}
+      <AnimatedStroke
+        progress={progress}
+        d={checkMarkPath}
+        stroke={checkMarkColor}
+        strokeWidth={8}
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        strokeOpacity={checked || false ? 1 : 0}
+      />
     </SVG>
   )
 }
