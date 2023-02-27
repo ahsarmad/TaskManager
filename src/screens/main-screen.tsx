9f906cfc9e8690e3fcab1fb6fe7e4358ce7a9a97
Sarmad <afzalsarmad77@gmail.com>
2023-02-26 20:11:59 -0500

    list component implemented

--- src/screens/main-screen.tsx
+++ src/screens/main-screen.tsx
@@ -1,51 +1,111 @@
 import React, { useCallback, useState } from 'react'
-import { Pressable } from 'react-native'
 import {
-  Text,
-  Box,
   Center,
   VStack,
   themeTools,
   useTheme,
   useColorMode,
-  useColorModeValue
+  useColorModeValue,
+  Fab,
+  Icon
 } from 'native-base'
+import { AntDesign } from '@expo/vector-icons'
+import AnimatedColorBox from '../components/animated-color-box'
 import ThemeToggle from '../components/theme-toggle'
-import AnimatedCheckBox from '../components/animated-checkbox'
-import TaskItem from '../components/task-item'
+import TaskList from '../components/task-list'
+import shortid from 'shortid'
+
+const initialData = [
+  {
+    id: shortid.generate(),
+    subject: 'Compile progress report for Monday',
+    done: false
+  },
+  {
+    id: shortid.generate(),
+    subject: 'Refine navigation architecture in feature branch',
+    done: false
+  }
+]
 
 export default function MainScreen() {
-  const [checked, setChecked] = useState(false)
-  const [subject, setSubject] = useState('Task Item')
-  const [isEditing, setIsEditing] = useState(false)
-  const handlePressCheckBox = useCallback(() => {
-    setChecked(prev => !prev)
+  const [data, setData] = useState(initialData)
+  const [editingItemId, setEditingItemId] = useState<string | null>(null)
+  const handleToggleTaskItem = useCallback(item => {
+    setData(prevData => {
+      const newData = [...prevData]
+      const index = prevData.indexOf(item)
+      newData[index] = {
+        ...item,
+        done: !item.done
+      }
+      return newData
+    })
   }, [])
+  const handleChangeTaskItemSubject = useCallback((item, newSubject) => {
+    setData(prevData => {
+      const newData = [...prevData]
+      const index = prevData.indexOf(item)
+      newData[index] = {
+        ...item,
+        subject: newSubject
+      }
+      return newData
+    })
+  }, [])
+  const handleFinishEditingTaskItem = useCallback(_item => {
+    setEditingItemId(null)
+  }, [])
+  const handlePressTaskItemLabel = useCallback(item => {
+    setEditingItemId(item.id)
+  }, [])
+
+  const handleRemoveItem = useCallback(item => {
+    setData(prevData => {
+      const newData = prevData.filter(i => i !== item)
+      return newData
+    })
+  })
+
   return (
-    <Center
-      _dark={{ bg: 'blueGray.900' }}
-      _light={{ bg: 'blueGray.50' }}
+    <AnimatedColorBox
       flex={1}
+      bg={useColorModeValue('warmGray.50', 'primary.900')}
+      w="full"
     >
       <VStack space={5} alignItems="center" w="full">
-        <TaskItem
-          isEditing={isEditing}
-          isDone={checked}
-          onToggleCheckbox={handlePressCheckBox}
-          subject={subject}
-          onPressLabel={() => setIsEditing(true)}
-          onChangeSubject={setSubject}
-          onFinishEditing={() => setIsEditing(false)}
+        <TaskList
+          data={data}
+          onToggleItem={handleToggleTaskItem}
+          onChangeSubject={handleChangeTaskItemSubject}
+          onFinishEditing={handleFinishEditingTaskItem}
+          onPressLabel={handlePressTaskItemLabel}
+          onRemoveItem={handleRemoveItem}
+          editingItemId={editingItemId}
         />
-        <Box
-          borderRadius={10}
-          p={10}
-          bg={useColorModeValue('red.500', 'yellow.500')}
-        >
-          <Text>Hello</Text>
-        </Box>
+
         <ThemeToggle />
       </VStack>
-    </Center>
+      <Fab
+        position="absolute"
+        renderInPortal={false}
+        size="sm"
+        icon={<Icon color="white" as={<AntDesign name="plus" />} size="sm" />}
+        colorScheme={useColorModeValue('blue', 'darkBlue')}
+        bg={useColorModeValue('blue.500', 'blue.400')}
+        onPress={() => {
+          const id = shortid.generate()
+          setData([
+            {
+              id,
+              subject: '',
+              done: false
+            },
+            ...data
+          ])
+          setEditingItemId(id)
+        }}
+      />
+    </AnimatedColorBox>
   )
 }
